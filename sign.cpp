45ef875a6646a5c09a87d2d36f0cce4db0a89115
Zhou YiBin[周益彬] <zyb7171@closeli.cn>
2016-12-07 15:16:55 +0800

    change base64 method

--- sign.cpp
+++ sign.cpp
@@ -4,6 +4,89 @@
 #include <openssl/pem.h>
 #include "common_tool.h"
 
+static const char b64_table[] = {
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+  'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+  'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+  'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+  'w', 'x', 'y', 'z', '0', '1', '2', '3',
+  '4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+
+char *b64_encode (const unsigned char *src, size_t len) {
+  int i = 0;
+  int j = 0;
+  char *enc = NULL;
+  size_t size = 0;
+  unsigned char buf[4];
+  unsigned char tmp[3];
+
+  // alloc
+  enc = (char *) malloc(0);
+  if (NULL == enc) { return NULL; }
+
+  // parse until end of source
+  while (len--) {
+    // read up to 3 bytes at a time into `tmp'
+    tmp[i++] = *(src++);
+
+    // if 3 bytes read then encode into `buf'
+    if (3 == i) {
+      buf[0] = (tmp[0] & 0xfc) >> 2;
+      buf[1] = ((tmp[0] & 0x03) << 4) + ((tmp[1] & 0xf0) >> 4);
+      buf[2] = ((tmp[1] & 0x0f) << 2) + ((tmp[2] & 0xc0) >> 6);
+      buf[3] = tmp[2] & 0x3f;
+
+      // allocate 4 new byts for `enc` and
+      // then translate each encoded buffer
+      // part by index from the base 64 index table
+      // into `enc' unsigned char array
+      enc = (char *) realloc(enc, size + 4);
+      for (i = 0; i < 4; ++i) {
+        enc[size++] = b64_table[buf[i]];
+      }
+
+      // reset index
+      i = 0;
+    }
+  }
+
+  // remainder
+  if (i > 0) {
+    // fill `tmp' with `\0' at most 3 times
+    for (j = i; j < 3; ++j) {
+      tmp[j] = '\0';
+    }
+
+    // perform same codec as above
+    buf[0] = (tmp[0] & 0xfc) >> 2;
+    buf[1] = ((tmp[0] & 0x03) << 4) + ((tmp[1] & 0xf0) >> 4);
+    buf[2] = ((tmp[1] & 0x0f) << 2) + ((tmp[2] & 0xc0) >> 6);
+    buf[3] = tmp[2] & 0x3f;
+
+    // perform same write to `enc` with new allocation
+    for (j = 0; (j < i + 1); ++j) {
+      enc = (char *) realloc(enc, size + 1);
+      enc[size++] = b64_table[buf[j]];
+    }
+
+    // while there is still a remainder
+    // append `=' to `enc'
+    while ((i++ < 3)) {
+      enc = (char *) realloc(enc, size + 1);
+      enc[size++] = '=';
+    }
+  }
+
+  // Make sure we have enough space to add '\0' character at end.
+  enc = (char *) realloc(enc, size + 1);
+  enc[size] = '\0';
+
+  return enc;
+}
 
 //二进制转换为base64编码
 char* base64(const void *input, int length)
@@ -52,8 +135,8 @@ static std::string sign(const char *private_key,
     //  goto safe_exit;
     //}
     bufio = BIO_new(BIO_s_file());
-    //BIO_read_filename(bufio, "rsa_private_key_pkcs8.pem");
-    BIO_read_filename(bufio, "rsa_private_key.pem");
+    BIO_read_filename(bufio, "rsa_private_key_pkcs8.pem");
+    //BIO_read_filename(bufio, "rsa_private_key.pem");
 
     rsa = PEM_read_bio_RSAPrivateKey(bufio, NULL, NULL, NULL);
     if (rsa == NULL) {
@@ -109,7 +192,9 @@ static std::string sign(const char *private_key,
     }
 
     //signStr = common_tool::base64_encode((const unsigned char*)sign, size);
-    signStr = base64((const unsigned char*)sign, size);
+    //std::cout << sign;
+    //signStr = base64((const unsigned char*)sign, size);
+    signStr = b64_encode((const unsigned char*)sign, size);
     EVP_MD_CTX_cleanup(&ctx);
     free(sign);
 
@@ -136,5 +221,9 @@ safe_exit:
 int main(int argc, char *argv[])
 {
     std::cout << sign("", "data to sign") << std::endl;
+    //sign("", "data to sign");
+    //std::string hello = "helloworld";
+    //std::cout << base64((void*)hello.c_str(), hello.length()) << std::endl;
+    //std::cout << b64_encode((const unsigned char*)hello.c_str(), hello.length()) << std::endl;
     return 0;
 }
